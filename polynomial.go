64919b92ef9cd2fe8667e29a54bf7f61555ee1d3
Coda Hale <coda.hale@gmail.com>
2013-12-01 17:01:31 -0800

    Fold small files into polynomial.go.

--- polynomial.go
+++ polynomial.go
@@ -1,5 +1,9 @@
 package sss
 
+import (
+	"io"
+)
+
 type polynomial []element
 
 func (p polynomial) degree() int {
@@ -10,6 +14,57 @@ func (p polynomial) eval(x element) (result element) {
 	// Horner's scheme
 	for i := 1; i <= len(p); i++ {
 		result = result.mul(x).add(p[len(p)-i])
+	}
+	return
+}
+
+// generates a random n-degree polynomial w/ a given x-intercept
+func randPoly(degree int, x element, rand io.Reader) (polynomial, error) {
+	result := make(polynomial, degree+1)
+	result[0] = x
+
+	buf := make([]byte, degree-1)
+	_, err := io.ReadFull(rand, buf)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := 1; i < degree; i++ {
+		result[i] = element(buf[i-1])
+	}
+
+	// the Nth term can't be zero, or else it's a (N-1) degree polynomial
+	for {
+		buf = make([]byte, 1)
+		_, err := io.ReadFull(rand, buf)
+		if err != nil {
+			return nil, err
+		}
+
+		if buf[0] != 0 {
+			result[degree] = element(buf[0])
+			return result, nil
+		}
+	}
+}
+
+type pair struct {
+	x, y element
+}
+
+// Lagrange interpolation
+func interpolate(points []pair, x element) (value element) {
+	for i, a := range points {
+		weight := element(1)
+		for j, b := range points {
+			if i != j {
+				top := x.sub(b.x)
+				bottom := a.x.sub(b.x)
+				factor := top.div(bottom)
+				weight = weight.mul(factor)
+			}
+		}
+		value = value.add(weight.mul(a.y))
 	}
 	return
 }
--- polynomial_test.go
+++ polynomial_test.go
@@ -1,6 +1,7 @@
 package sss
 
 import (
+	"bytes"
 	"testing"
 )
 
@@ -22,5 +23,74 @@ func TestPolyEval(t *testing.T) {
 	actual := p.eval(2)
 	if actual != expected {
 		t.Errorf("Expected %v but was %v", expected, actual)
+	}
+}
+
+func TestRandPoly(t *testing.T) {
+	b := []byte{1, 2, 3}
+
+	expected := polynomial{10, 1, 2, 3}
+	actual, err := randPoly(3, 10, bytes.NewReader(b))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !equal(expected, actual) {
+		t.Errorf("Expected %v but was %v", expected, actual)
+	}
+}
+
+func TestRandPolyEOF(t *testing.T) {
+	b := []byte{1}
+
+	p, err := randPoly(3, 10, bytes.NewReader(b))
+	if p != nil {
+		t.Errorf("Expected an error but got %v", p)
+	}
+
+	if err == nil {
+		t.Error("No error returned")
+	}
+}
+
+func TestRandPolyEOFFullSize(t *testing.T) {
+	b := []byte{1, 2, 0, 0, 0, 0}
+
+	p, err := randPoly(3, 10, bytes.NewReader(b))
+	if p != nil {
+		t.Errorf("Expected an error but got %v", p)
+	}
+
+	if err == nil {
+		t.Error("No error returned")
+	}
+}
+
+func TestRandPolyFullSize(t *testing.T) {
+	b := []byte{1, 2, 0, 4}
+
+	expected := polynomial{10, 1, 2, 4}
+	actual, err := randPoly(3, 10, bytes.NewReader(b))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !equal(expected, actual) {
+		t.Errorf("Expected %v but was %v", expected, actual)
+	}
+}
+
+func TestInterpolation(t *testing.T) {
+	expected := element(0)
+	actual := interpolate(
+		[]pair{
+			pair{x: 1, y: 1},
+			pair{x: 2, y: 2},
+			pair{x: 3, y: 3},
+		},
+		0)
+
+	if expected != actual {
+		t.Errorf("Expected %v, but was %v", expected, actual)
 	}
 }
